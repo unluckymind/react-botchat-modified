e50ed299a619bbe94da8c80ed85f33b5c0f28eb3
deaaprizal <dea.aprizal@icloud.com>
2019-07-04 16:40:18 +0700

    sizing dots, removing text loading

--- src/components/Messages/TextMessage.js
+++ src/components/Messages/TextMessage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './loading.css'
 // import chatIconUrl from './../../assets/chat-icon.svg';
 
 class TextMessage extends Component {
@@ -9,28 +10,41 @@ class TextMessage extends Component {
       showloader: false,
       text: this.props.message.data.text || '',
       author: this.props.message.author,
-      time: this.props.message.time
+      time: this.props.message.time,
+      onHittingApi: this.props.message.load
     }
   }
 
+  // shouldComponentUpdate(a, b){
+  //   if(b.onHittingApi){
+  //     return true
+  //   }else{
+  //     return false
+  //   }
+  // }
+
+  // componentDidUpdate(prevProp){
+
+  // }
+
   componentDidMount(){
-    console.log('perubahan: ')
     setTimeout( () => { 
       if(this.state.loading){
         this.setState({showloader: true})
       }
-    }, 3000)
+    }, 5000)
   }
 
   render(){
-    const {text, author, time, showloader} = this.state
+    const {text, author, time, showloader, onHittingApi, loading} = this.state
     return(
-        !showloader && author =="them" ? <span style={{fontSize: "24px"}}>. . .</span> : 
+        !showloader && author == "them" ? <span className="loading"></span> : 
         <div className="sc-message--text">{text}
         <div className="sc-message--meta">{time}</div>
         </div>
     )
   }
 }
+
 
 export default TextMessage
